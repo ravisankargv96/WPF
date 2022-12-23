bccc6ecdec98f260f52e202fdbab41e37e29c262
ravisankargv96 <ravisankargv96@gmail.com>
2022-12-24 03:43:21 +0530

    Add project files.

---
+++ 4. Data Binding/DataBinding/4.1.ASimplePersonEditorLayout/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ASimplePersonEditorLayout
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person person = new Person("Tom", 9);
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //object -> UI
+            this.nameTextBox.Text = person.Name;
+            this.ageTextBox.Text = person.Age.ToString();
+
+            this.birthdayButton.Click += birthdayButton_Click;
+        }
+
+        void birthdayButton_Click(object sender, RoutedEventArgs e) {
+            ++person.Age;
+            MessageBox.Show(
+                string.Format($"Happy Birthday, {person.Name}, age {person.Age}!"),"Birthday"
+                );
+        }
+    }
+}
---
+++ 4. Data Binding/DataBinding/4.5.AClassThatSupportsPropertyChangeNotification/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AClassThatSupportsPropertyChangeNotificationAndControlChanges
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Person person = new Person("Tom", 9);
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //object -> UI
+            this.nameTextBox.Text = person.Name;
+            this.ageTextBox.Text = person.Age.ToString();
+
+            this.birthdayButton.Click += birthdayButton_Click;
+
+            person.PropertyChanged += person_PropertyChanged;
+
+            this.nameTextBox.TextChanged += nameTextBox_TextChanged;
+            this.ageTextBox.TextChanged += ageTextBox_TextChanged;
+        }
+
+        void person_PropertyChanged(object sender, PropertyChangedEventArgs e) {
+
+            switch (e.PropertyName) {
+                case "Name":
+                    this.nameTextBox.Text = person.Name;
+                    break;
+
+                case "Age":
+                    this.ageTextBox.Text = person.Age.ToString();
+                    break;
+            }
+        }
+
+        void birthdayButton_Click(object sender, RoutedEventArgs e)
+        {
+            ++person.Age; // person_PropertyChanged will update ageTextBox
+
+            MessageBox.Show(
+                string.Format($"Happy Birthday, {person.Name}, age {person.Age}!"), "Birthday"
+                );
+        }
+
+        void nameTextBox_TextChanged(object sender, TextChangedEventArgs e) { 
+            person.Name = nameTextBox.Text;
+        }
+
+        void ageTextBox_TextChanged(object sender, TextChangedEventArgs e) {
+
+            int age = 0;
+            if(int.TryParse(ageTextBox.Text, out age))
+            {
+                person.Age = age;
+            }
+        }
+
+    }
+}
